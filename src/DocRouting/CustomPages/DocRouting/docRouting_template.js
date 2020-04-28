80c99a47897417144be850bebdce1972d4666101
marc@crmtogether.com <marc@crmtogether.com>
2020-04-28 10:01:00 +0100

    fixed for recent values

--- src/DocRouting/CustomPages/DocRouting/docRouting_template.js
+++ src/DocRouting/CustomPages/DocRouting/docRouting_template.js
@@ -7,6 +7,46 @@ function openSendDocument() {
 crm.ready(function() {
 	var enableForEntity = [!ENTITIES];
 	var T = crm.getArg("T",crm.url()).toLowerCase();
+	if (T=="")
+	{
+		var RecentValue=crm.getArg('RecentValue', crm.url());	
+		var _url=new String(crm.url());
+		if (RecentValue!="")
+		{
+			if (_url.indexOf("RecentValue=200X")>0)
+			{
+				T="company";
+			}else 
+			if (_url.indexOf("RecentValue=220X")>0)
+			{
+				T="person";
+			}else 
+			if (_url.indexOf("RecentValue=200X")>0)
+			{
+				T="company";
+			}else 
+			if (_url.indexOf("RecentValue=281X")>0)
+			{
+				T="case";
+			}else 
+			if (_url.indexOf("RecentValue=260X")>0)
+			{
+				T="opportunity";
+			}else 
+			if (_url.indexOf("RecentValue=1469X")>0)
+			{
+				T="quote";
+			}else 
+			if (_url.indexOf("RecentValue=1463X")>0)
+			{
+				T="order";
+			}else 
+			if (_url.indexOf("RecentValue=1284X")>0)
+			{
+				T="solutions";
+			}				
+		}
+	}
 	var key = crm.getArg('Key0', crm.url());	
 	if ($.inArray( T, enableForEntity) > -1) {		
 		crm.addButton("Send.gif","Email " +  T + " document", "Email " +  T + " Document","javascript:openSendDocument()");
